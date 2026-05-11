0f9509d4e151aaa064dfb1d386832f96542eba24
vq4qh6trq2-sys <vq4qh6trq2@privaterelay.appleid.com>
2026-05-11 16:13:40 +0200

    Delete page.tsx

--- page.tsx
+++
@@ -1,8 +0,0 @@
-export const metadata = { title: 'LightAO' }
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="fr">
-      <body>{children}</body>
-    </html>
-  )
-}
